8291fe4deb8a8c2ba941b61bd09bc97ac6ac770c
tarroyo0907 <tarroyo0907@gmail.com>
2025-11-09 15:55:52 -0500

    Added Physical Doors instead of Shop Menu. Can be purchased and opened.

--- UnityProject/So Live Service/Assets/Scripts/Interactions/PlayerInteraction.cs
+++ UnityProject/So Live Service/Assets/Scripts/Interactions/PlayerInteraction.cs
@@ -1,3 +1,5 @@
+using Unity.VisualScripting;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
@@ -5,38 +7,79 @@ using UnityEngine.InputSystem.Controls;
 public class PlayerInteraction : MonoBehaviour
 {
     // Delegates
-    public delegate void PlayerButtonInteractionDelegate();
+    public delegate void PlayerInteractionDelegate();
+
     // Events
-    public static event PlayerButtonInteractionDelegate OnButtonInteraction;
+    public static event PlayerInteractionDelegate OnButtonInteraction;
+    public static event PlayerInteractionDelegate OnShopInteraction;
 
     // References
     [SerializeField] private InputActionReference interactionAction;
+    [SerializeField] private InputActionReference shopInputAction;
 
     // Data
     [SerializeField] private float currentBalance = 0.0f;
+    [SerializeField] private GameObject currentInteractingObject = null;
 
     // Properties
     public float CurrentBalance { get => currentBalance; set { currentBalance = value; } }
+    public GameObject CurrentInteractingObject { get => currentInteractingObject; set {  currentInteractingObject = value; } }
 
     private void OnEnable()
     {
         if (interactionAction != null && interactionAction.action != null)
             interactionAction.action.performed += OnInteractionPerformed;
+
+        if (shopInputAction != null && shopInputAction.action != null)
+            shopInputAction.action.performed += OnShopInteractionPerformed;
     }
 
     private void OnDisable()
     {
         if (interactionAction != null && interactionAction.action != null)
             interactionAction.action.performed -= OnInteractionPerformed;
+
+        if (shopInputAction != null && shopInputAction.action != null)
+            shopInputAction.action.performed -= OnShopInteractionPerformed;
     }
 
     private void OnInteractionPerformed(InputAction.CallbackContext ctx)
     {
+        ButtonControl button = (ButtonControl) ctx.control;
         // Ensure this came from the mouse left button (ignore other bindings/devices)
-        if (ctx.control is ButtonControl button && button == Mouse.current.leftButton)
+        if (button == Mouse.current.leftButton)
         {
             ButtonInteraction();
         }
+
+        if (button == Keyboard.current.eKey)
+        {
+            if (currentInteractingObject != null)
+            {
+                switch (currentInteractingObject.name)
+                {
+                    case "DoorTriggerCollider":
+                        if (currentBalance >= 10)
+                        {
+                            currentInteractingObject.GetComponent<Door>().OpenDoor();
+                            currentBalance -= 10;
+                        }
+                        else
+                        {
+                            Debug.Log("Not Enough Money to Open Door!");
+                        }
+          
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    private void OnShopInteractionPerformed(InputAction.CallbackContext ctx)
+    {
+        OnShopInteraction.Invoke();
     }
 
     private void ButtonInteraction()
