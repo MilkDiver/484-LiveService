3da151524e6c7814bc75a082195d0310eee61a19
SleepyGameZs <76854561+SleepyGameZs@users.noreply.github.com>
2025-12-02 09:36:14 -0500

    Pushing Changes before Pulling to my Branch

---
+++ UnityProject/So Live Service/Assets/Scripts/Interactions/PickUp.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PickUp : MonoBehaviour, IInteractable
+{
+    public string InteractMessage => throw new System.NotImplementedException();
+
+    public void Interact()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    [SerializeField] private GameObject _pickedUpItem;
+
+    [SerializeField] private GameObject _holdLocation;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void PickUpItem()
+    {
+
+    }
+
+    private void DropItem()
+    {
+
+    }
+
+    private void 
+}
